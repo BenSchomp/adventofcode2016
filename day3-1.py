b8d1c71f73ad76519e33be8cc7e430b25396cdc0
Ben Schomp <ben@benschomp.com>
2016-12-04 23:35:33 -0500

    day3 success part 1

---
+++ day3-1.py
@@ -0,0 +1,12 @@
+file = open('day3-input.txt', 'r')
+
+valid = 0
+for line in file:
+  sides = map( int, line.split() )
+  sides.sort()
+  if sides[0] + sides[1] > sides[2]:
+    valid += 1
+
+
+print "valid: ", valid
+file.close()
