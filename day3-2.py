430cf5c9460e1e1bf9ef29f0247be2d7ed8d6e0a
Ben Schomp <ben@benschomp.com>
2016-12-04 23:53:08 -0500

    ...here it is...

---
+++ day3-2.py
@@ -0,0 +1,26 @@
+file = open('day3-input.txt', 'r')
+
+def isValid( sides ):
+  sides.sort()
+  return sides[0] + sides[1] > sides[2]
+
+valid = 0
+
+while True:
+  a = file.readline()
+  b = file.readline()
+  c = file.readline()
+
+  if a == '' or b == '' or c == '':
+    break
+
+  aRow = map( int, a.split() )
+  bRow = map( int, b.split() )
+  cRow = map( int, c.split() )
+
+  for i in range( 3 ):
+    if isValid( [ aRow[i], bRow[i], cRow[i] ] ):
+      valid += 1
+
+print "valid: ", valid
+file.close()
