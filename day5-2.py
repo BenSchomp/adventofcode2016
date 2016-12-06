e4398a0083a6741af8cb796a90b61c8a056ff567
Ben Schomp <ben@benschomp.com>
2016-12-06 00:01:35 -0500

    day 5 part 2 - success

---
+++ day5-2.py
@@ -0,0 +1,21 @@
+import hashlib
+
+#doorid = 'abc' # 05ace8e3
+doorid = 'ffykfhsq'
+
+index = 0
+found = 0
+password = list('________')
+while found < 8:
+  m = hashlib.md5()
+  m.update( doorid + str(index) )
+  h = m.hexdigest()
+  if h[0:5] == '00000':
+    pos = h[5]
+    if pos >= '0' and pos < '8' and password[int(pos)] == '_':
+      password[int(pos)] = h[6]
+      print doorid + str(index), ''.join( password )
+      found += 1
+  index += 1
+
+print "...", "Password:", ''.join( password )
