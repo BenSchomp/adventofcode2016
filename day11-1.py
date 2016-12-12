90f885c7bc0d93b82f1fe3595ea2c6942300e659
Ben Schomp <ben@benschomp.com>
2016-12-12 15:23:33 -0800

    runs through but doesn't find shortest path yet

--- day11-1.py
+++ day11-1.py
@@ -1,113 +1,146 @@
+from copy import deepcopy
+
 file = open('day11-example.txt', 'r')
 #file = open('day11-input.txt', 'r')
 
-seen = {}
+# floors = { 0: cur, 1: [ ], 2: [ ], 3: [ ], 4: [ ] }
 
-def move( floors, which, cur, new ):
-  print 'move before:', floors, which, cur, new
-  for i in which:
+def getCur( floors ):
+  return floors[0][0]
+
+def setCur( floors, cur ):
+  floors[0][0] = cur
+
+def display( floors, text = '' ):
+  print text
+  for i in range( 4, 0, -1 ):
+    print 'f%d:' % i,
+    print '*' if getCur( floors ) == i else ' ',
+    print ' '.join( floors[i] )
+
+def move( floors, items, to ):
+  items.sort( reverse=True )
+  cur = getCur( floors )
+  for i in items:
     tmp = floors[cur][i]
     floors[cur].remove( tmp )
-    floors[new].append( tmp )
-    floors[new].sort()
-  print 'move after:', floors
+    floors[to].append( tmp )
+    floors[to].sort()
+  setCur( floors, to )
 
-  return floors
-  
+seen = {}
+def solve( floors, steps ):
+  #display( floors, '+ solve:' )
 
-def solve( floors, cur, steps ):
   # seen this position before?
   global seen
-  print '+ solve:', floors, ', cur:', cur, ', steps:', steps
+  cur = getCur( floors )
+  numItems = len( floors[cur] )
+
   pos = ''
-  for floor in floors:
+  for floor in floors[1:]:
     pos += ''.join( floor ) + '-'
-  pos += '-' + str( cur )
+  pos += str( cur )
 
   if pos in seen:
-    print '! seen this position before'
+    #print '! seen this position before'
     return -1
   else:
     seen[pos] = 1
 
   # puzzle solved?
-  if len( floors[0] ) + len( floors[1] ) + len( floors[2] ) == 0:
-    print '! solution found'
+  if len( floors[1] ) + len( floors[2] ) + len( floors[3] ) == 0:
+    print '*** solution found (%d) ***' % steps
+    print floors
     return steps
   
   # match up microchips with generators
-  tmp = floors[cur]
+  tmp = deepcopy( floors[cur] )
+  chips = []
+  generators = []
   for i in tmp:
     if i[1] == 'm':
-      for j in tmp:
-        if j[0] == i[0] and j[1] == 'g':
-          tmp.remove( j[0] + 'm' )
-          tmp.remove( j[0] + 'g' )
+      chips.append( i )
+    elif i[1] == 'g':
+      generators.append( i )
+    else:
+      exit()
 
-  # now any chips left are fried if any generators are present
-  hasChip = False
-  for i in tmp:
-    if i[1] == 'm':
-      hasChip = True
+  matches = 0
+  for i in chips:
+    for j in generators:
+      if i[0] == j[0]:
+        matches += 1
+        break
 
-    if hasChip and i[1] == 'g':
-      # fried
-      print '! fried', tmp
-      return -1
-
+  if len( generators ) > matches and len( chips ) > matches:
+    #print '! fried', tmp
+    return -1
+  
   # try moving each piece up and then down and both dirs in pairs
-  print ' -=- move singles -=-'
-  for i in range( 0, len( floors[cur] ) ):
+  orig = deepcopy( floors )
+  for i in range( 0, numItems ):
     # up
-    if cur < 3:
-      #solve( move( floors, [i], cur, cur+1 ), cur+1, steps )
-      print 'before', floors
-      move( floors[:], [i], cur, cur+1 )
-      print 'after', floors
+    if cur < 4:
+      floors = deepcopy( orig )
+      move( floors, [i], cur+1 )
+      result = solve( floors, steps+1 )
+      if result > 0:
+        return result
 
     #down
-    #solve( move( floors, [i]cur, cur-1 ), cur-1, steps )
+    if cur > 1:
+      floors = deepcopy( orig )
+      move( floors, [i], cur-1 )
+      result = solve( floors, steps+1 )
+      if result > 0:
+        return result
 
-  i = 0
-  j = 0
-  for i in range( 0, len( floors[cur] ) ):
-    for j in range( i+1, len( floors[cur] ) ):
-      print i, j
+  for i in range( 0, numItems ):
+    for j in range( i+1, numItems ):
       # up
-      #solve( move( floors, [i, j], cur, cur+1 ), cur+1, steps )
+      if cur < 4:
+        floors = deepcopy( orig )
+        move( floors, [i, j], cur+1 )
+        result = solve( floors, steps+1 )
+        if result > 0:
+          return result
 
       #down
-      #solve( move( floors, [i, j]cur, cur-1 ), cur-1, steps )
+      if cur > 1:
+        floors = deepcopy( orig )
+        move( floors, [i, j], cur-1 )
+        result = solve( floors, steps+1 )
+        if result > 0:
+          return result
 
-  return floor
+  return -1
 
 # initial setup
-print ' -=- Init -=-'
-floors =[ [] for i in range(1, 5) ]
-f = 0
+floors =[ [] for i in range(0, 5) ]
+floors[0].append( 1 )
+i = 1
 for line in file:
   line = line.rstrip()
 
   parts = line.split( ' a ' )
   for part in parts[1:]:
     subParts = part.split()
-    print ' %d. ' % f,
     if subParts[1][0:9] == 'generator':
       code = subParts[0][0] + 'g'
-      print code
     elif subParts[1][0:9] == 'microchip':
       code = subParts[0][0] + 'm'
-      print code
 
-    floors[f].append( code )
-    floors[f].sort()
-  f += 1
+    floors[i].append( code )
+    floors[i].sort()
+  i += 1
 
 file.close()
+#display( floors, '+ Init' )
 
 print ' -=- begin -=-'
-print floors
-answer = solve( floors, 0, 0 )
+display( floors )
+answer = solve( floors, 0 )
 print ' -=- done -=-'
 print answer
 
