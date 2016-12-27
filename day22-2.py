0f0b124571338cc326369f69c5af06773092cb16
Ben Schomp <ben@benschomp.com>
2016-12-27 08:57:40 -0800

    this is pretty hacky but I'm ok with it

--- day22-2.py
+++ day22-2.py
@@ -1,7 +1,6 @@
 from copy import deepcopy
 from collections import deque
 
-
 # -=-=- HELPERS -=-=-
 
 version = "examplex"
@@ -17,7 +16,7 @@ else:
 loOutlier = 0
 hiOutlier = 0
 
-def display( n ):
+def display_old( n ):
   for y in range( HEIGHT ):
     r = ''
     for x in range( WIDTH ):
@@ -36,7 +35,7 @@ def getChar( n, cur, goal ):
     ch = 'G'
   else:
     if used < loOutlier:
-      ch = '_'
+      ch = 'X'
     elif size > hiOutlier:
       ch = '#'
     else:
@@ -52,11 +51,15 @@ def getKey( n, goal ):
 
   return r
 
-def display2( n, goal ):
+def display( n, goal ):
+  print
   for y in range( HEIGHT ):
     r = ''
     for x in range( WIDTH ):
-      r += getChar( n, (x,y), goal )
+      if False and x == 0 and y == 0:
+        r += 'O'
+      else:
+        r += getChar( n, (x,y), goal )
 
     print r
 
@@ -65,6 +68,8 @@ def display2( n, goal ):
 nodes = [[ -1 for x in range(WIDTH)] for y in range(HEIGHT)]
 maxX = 0
 maxY = 0
+emptyX = 0
+emptyY = 0
 sizeTotal = 0
 count = 0
 for line in file:
@@ -85,6 +90,10 @@ for line in file:
   size = int( parts[1][:-1] )
   sizeTotal += size
 
+  if used == 0:
+    emptyX = x
+    emptyY = y
+
   nodes[y][x] = [ used, size ]
 
 file.close()
@@ -98,11 +107,78 @@ seen = dict()
 
 # -=-=- DO IT -=-=-
 
-cur = (maxX, 0 )
-display2( nodes, cur )
+curX = maxX
+curY = 0
+moves = 0
+display( nodes, (curX, curY) )
 
-key = getKey( nodes, cur )
+key = getKey( nodes, (curX, curY) )
 work.append( key )
 seen[key] = 1
+
+def move( fromX, fromY, toX, toY ):
+  global moves
+  if toX < 0 or toX > WIDTH-1 or toY < 0 or toY > HEIGHT:
+    print "OUT OF BOUNDS (%d,%d) to (%d,%d)" % (fromX,fromY,toX,toY)
+    exit()
+
+  fromUsed = nodes[fromY][fromX][0]
+  fromSize = nodes[fromY][fromX][1]
+  toUsed = nodes[toY][toX][0]
+  toSize = nodes[toY][toX][1]
+
+  if fromUsed > toSize or toUsed > fromSize:
+    return False
+
+  tmpNode = nodes[fromY][fromX]
+  nodes[fromY][fromX] = nodes[toY][toX]
+  nodes[toY][toX] = tmpNode
+
+  moves += 1
+
+  return True
+
+# move empty space up to the top row
+while emptyY > 0:
+  if move( emptyX, emptyY, emptyX, emptyY-1 ):
+    emptyY -= 1
+    display( nodes, (curX, curY) )
+  else:
+    if move( emptyX, emptyY, emptyX-1, emptyY ):
+      emptyX -=1
+      display( nodes, (curX, curY) )
+    else:
+      print "NOW WHAT?"
+      exit()
+
+# move empty space right to the goal
+while emptyX < curX - 1:
+  if not move( emptyX, emptyY, emptyX+1, emptyY ):
+    print "UH OH!"
+    exit()
+
+  emptyX += 1
+  display( nodes, (curX, curY) )
+
+while True:
+  if move( emptyX, emptyY, emptyX+1, emptyY ):
+    emptyX += 1
+    curX -= 1
+    display( nodes, (curX, curY) )
+    if curX < 1:
+      print "Done: ", moves
+      exit()
+  if move( emptyX, emptyY, emptyX, emptyY+1 ):
+    emptyY += 1
+    display( nodes, (curX, curY) )
+  if move( emptyX, emptyY, emptyX-1, emptyY ):
+    emptyX -= 1
+    display( nodes, (curX, curY) )
+  if move( emptyX, emptyY, emptyX-1, emptyY ):
+    emptyX -= 1
+    display( nodes, (curX, curY) )
+  if move( emptyX, emptyY, emptyX, emptyY-1 ):
+    emptyY -= 1
+    display( nodes, (curX, curY) )
 
 
